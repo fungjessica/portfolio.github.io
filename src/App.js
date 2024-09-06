e78041eca3824526c27053d25a3c2b029d5d1b42
jessimoo <fung.jessica03@gmail.com>
2024-09-05 22:27:20 -0700

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+{/*import Navbar from './components/Navbar';
+import Home from './components/home';
+import About from './components/about';
+import Projects from './components/projects';
+import Resume from './components/resume';
+import Contact from './components/contact';*/}
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar>
+        </Navbar>
+          {/*<Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/resume' element={<Resume />} />
+              <Route path='/contact' element={<Contact />} />
+          </Routes>*/}
+      </div>
+    </BrowserRouter>
   );
 }
 
